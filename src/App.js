f3d0fdf1526e66a7f200879c93331f50c1db2d31
Jyoti Sahu <sahujyoti50@gmail.com>
2022-06-02 12:52:21 +0200

    flip-image-app pure CSS and HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,155 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/*two-sided Horizontal-Flip-effect*/}
+      <h3>Two-sided Vertical-Flip-Effect</h3>
+      <div style={{ display: "flex", width: "inherit" }}>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="yellow_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="yellow_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="blue_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="blue_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="white_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="white_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="green_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="green_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="food_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="food_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="taste_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="taste_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="red_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="red_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <h3>Two-sided Horizontal-Flip-Effect</h3>
+      <div style={{ display: "flex", width: "inherit" }}>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="yellow_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="yellow_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="blue_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="blue_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="white_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="white_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="green_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="green_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="food_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="food_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="taste_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="taste_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+        <div class="vertical two-sided-effect">
+          <div class="flipper">
+            <div class="front">
+              <img src="red_front.jpg" alt="front" />
+            </div>
+            <div class="back">
+              <img src="red_back.jpg" alt="back" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
